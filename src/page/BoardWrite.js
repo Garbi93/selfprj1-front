e42bab3bfbd7cae2808679a5d5efb90cc33cfa10
Garbi93 <iseung2093@gmail.com>
2023-11-18 12:53:41 +0900

    11/18 게시물 작성 폼 작성

--- src/page/BoardWrite.js
+++ src/page/BoardWrite.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { Box, FormControl, FormLabel, Input, Textarea } from "@chakra-ui/react";
 
 export function BoardWrite() {
-  return <h1>게시물 작성</h1>;
+  return (
+    <Box>
+      <h1>게시물 작성</h1>
+      <Box>
+        <FormControl>
+          <FormLabel>제목</FormLabel>
+          <Input />
+        </FormControl>
+        <FormControl>
+          <FormLabel>본문</FormLabel>
+          <Textarea />
+        </FormControl>
+        <FormControl>
+          <FormLabel>글 작성자</FormLabel>
+          <Input />
+        </FormControl>
+      </Box>
+    </Box>
+  );
 }
