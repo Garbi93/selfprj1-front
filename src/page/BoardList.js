467e82eeca1d3dfd5d5d46ca44bfe15626ce6ef1
Garbi93 <iseung2093@gmail.com>
2023-11-18 15:54:42 +0900

    11/18 홈화면이 띄워지면 바로 게시글 목록 들을 조회 하기 위해 'useEffect' 를 사용 하였고 get요청으로 "/api/board/list" 경로 지정 후 해당 경로에서 받은 파일을 boardList에 저장하여 조회 되도록 코드 작성

--- src/page/BoardList.js
+++ src/page/BoardList.js
@@ -1,5 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Box, Spinner, Table, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import axios from "axios";
 
 export function BoardList() {
-  return <h1>게시물 목록</h1>;
+  const [boardList, setBoardList] = useState(null);
+
+  // 버튼이나 무슨 행위를 변경할때 요청 하는것이 아닌
+  // 페이지가 조회 될때나 어떻한 상태가 변경 되었을 때에 변경을 원하면
+  // useEffect 를 사용 한다.
+  useEffect(() => {
+    // 게시글을 작성한 것들을 조회 하고 싶어 get 메소드를 요청
+    axios
+      .get("/api/board/list") // back의 해당 경로로 요청
+      // 잘 받았으면 받아온 data 를 setBoardList 로 boardList 에 받아온 값들을 저장
+      .then((response) => setBoardList(response.data)); //
+  }, []);
+
+  return (
+    <Box>
+      <h1>게시물 목록</h1>
+      <Box>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>id</Th>
+              <Th>title</Th>
+              <Th>nickName</Th>
+              <Th>inserted</Th>
+            </Tr>
+          </Thead>
+          {boardList || <Spinner />}
+          {boardList &&
+            boardList.map((board) => {
+              <Tr>
+                <Td>{board.id}</Td>
+                <Td>{board.title}</Td>
+                <Td>{board.writer}</Td>
+                <Td>{board.inserted}</Td>
+              </Tr>;
+            })}
+        </Table>
+      </Box>
+    </Box>
+  );
 }
