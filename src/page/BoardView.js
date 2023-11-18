f046346b744f140138ca0855b486e6414b0719a8
Garbi93 <iseung2093@gmail.com>
2023-11-18 17:03:33 +0900

    11/18 해당 게시글 삭제 프론트 작성

--- src/page/BoardView.js
+++ src/page/BoardView.js
@@ -1,10 +1,29 @@
-import { Box, Spinner } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spinner,
+  Textarea,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 export function BoardView() {
   const [board, setBoard] = useState(null);
+
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
   const { id } = useParams();
 
   useEffect(() => {
@@ -17,14 +36,56 @@ export function BoardView() {
     return <Spinner />;
   }
 
+  function handleDelete() {
+    axios
+      .delete("/api/board/remove/" + id)
+      .then(() => console.log("deleted"))
+      .catch(() => console.log("delete fail"))
+      .finally(() => console.log("dDone"));
+  }
+
   return (
     <Box>
-      <h1>글 보기</h1>
-      <p>번호 : {board.id}</p>
-      <p>제목 : {board.title}</p>
-      <p>내용 : {board.content}</p>
-      <p>작성자 : {board.writer}</p>
-      <p>작성일 : {board.inserted}</p>
+      <FormControl>
+        <FormLabel>글 번호</FormLabel>
+        <Input value={board.id} readOnly />
+      </FormControl>
+      <FormControl>
+        <FormLabel>제목</FormLabel>
+        <Input value={board.title} readOnly />
+      </FormControl>
+      <FormControl>
+        <FormLabel>본문</FormLabel>
+        <Textarea value={board.content} readOnly />
+      </FormControl>
+      <FormControl>
+        <FormLabel>작성자</FormLabel>
+        <Input value={board.writer} readOnly />
+      </FormControl>
+      <FormControl>
+        <FormLabel>작성일</FormLabel>
+        <Input value={board.inserted} readOnly />
+      </FormControl>
+      <Button colorScheme={"purple"}>수정</Button>
+      <Button colorScheme={"red"} onClick={onOpen}>
+        삭제
+      </Button>
+
+      {/* 삭제 모달 */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>삭제 확인</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>삭제 하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button onClick={handleDelete} colorScheme={"red"}>
+              삭제
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
