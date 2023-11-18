b8daad09f3c4f2f8b0783a9582ce122c1c923415
Garbi93 <iseung2093@gmail.com>
2023-11-18 13:05:18 +0900

    11/18 게시물 작성 의 각 폼에 input 데이터 입력시 state 변경 되도록 useState를 사용 하고 '저장' 버튼 클릭시 'handleBoardSave' 기능을 추가하여 axios로 post 요청을 backEnd에 보내고 잘 받으면 'good'출력 아니면 'bad'출력 하고 잘 받던 못받던 axios가 실행 되면 'done'을 출력하도록 코드 작성

--- src/page/BoardWrite.js
+++ src/page/BoardWrite.js
@@ -1,23 +1,49 @@
-import React from "react";
-import { Box, FormControl, FormLabel, Input, Textarea } from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
+import axios from "axios";
 
 export function BoardWrite() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [writer, setWriter] = useState("");
+
+  function handleBoardSave() {
+    axios
+      .post("/api/board/add", { content, writer })
+      .then(() => console.log("good"))
+      .catch(() => console.log("bad"))
+      .finally(() => console.log("done"));
+  }
+
   return (
     <Box>
       <h1>게시물 작성</h1>
       <Box>
         <FormControl>
+          <FormLabel>글 작성자</FormLabel>
+          <Input value={writer} readOnly />
+        </FormControl>
+        <FormControl>
           <FormLabel>제목</FormLabel>
-          <Input />
+          <Input value={title} onChange={(e) => setTitle(e.target.value)} />
         </FormControl>
         <FormControl>
           <FormLabel>본문</FormLabel>
-          <Textarea />
+          <Textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
         </FormControl>
-        <FormControl>
-          <FormLabel>글 작성자</FormLabel>
-          <Input />
-        </FormControl>
+        <Button onClick={handleBoardSave} colorScheme={"blue"}>
+          저장
+        </Button>
       </Box>
     </Box>
   );
