17ddfe24a8c6eeba5c9418d88cf43396d1d0c796
Garbi93 <iseung2093@gmail.com>
2023-11-18 12:49:59 +0900

    11/18 routes를 생성 해주고 routes 내부에 경로 들을 지정 경로당 사용하는 component 들을 넣어주고 App component에 RouterProvider 로 routes를 사용 하도록 코드 업데이트 함

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { BoardWrite } from "./page/BoardWrite";
+import { BoardList } from "./page/BoardList";
+import { HomeLayout } from "./layout/HomeLayout";
+
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    // 전체 홈 레이 아웃 에서 위치를 이동해도 변경되지 않는 부분을 보여줄 것을 HomeLayout component 에 작성 해줄 예정
+    <Route path="/" element={<HomeLayout />}>
+      {/* path를 부모의 경로를 그대로 받으면 index를 써준다. */}
+      {/* 홈페이지가 처음 보이면 보일때에 BoardList 를 보여줄 예정이라 사용함 */}
+      {/* BoardList 와 BoardWrite 등 페이지가 전환되면 보여줄 component 들은 page 폴더 내부에 옮겨 저장 함 */}
+      <Route index element={<BoardList />} />
+      {/* write라는 경로로 이동하게 되면 게시물 작성 을 할 수 있게 BoardWrite component 를 작성 함 */}
+      <Route path="write" element={<BoardWrite />} />
+    </Route>,
+  ),
+);
 
 function App() {
-  return (
-    <div></div>
-  );
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
