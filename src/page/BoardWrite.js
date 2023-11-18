d312cf9bfc07569c32d1f5b8f2d263cf327880e9
Garbi93 <iseung2093@gmail.com>
2023-11-18 15:22:38 +0900

    11/18 백엔드로 부터 받은 error 코드 (badRequest, internalServerError)등을 받을 때 error 로 인자를 받아  error.response.status 코드 값을 뽑아내어 원하는 toast를 출력 시킨다.

--- src/page/BoardWrite.js
+++ src/page/BoardWrite.js
@@ -26,11 +26,21 @@ export function BoardWrite() {
           status: "success",
         });
       })
-      .catch(() => {
-        toast({
-          description: "게시물 저장 중 문제가 발생하였습니다.",
-          status: "error",
-        });
+      // 백엔드로 부터 받은 error 코드 (badRequest, internalServerError)등을 받을 때 error 로 인자를 받는다.
+      .catch((error) => {
+        // 받은 error 의 응답의 상태값을 뽑아 코드가 400과 같으면 원하는 toast를 띄우게 한다.
+        if (error.response.status === 400) {
+          toast({
+            title: "작성한 게시물에 빈 값이 있습니다.",
+            description: "내용을 추가 해주세요.",
+            status: "error",
+          });
+        } else {
+          toast({
+            description: "게시물 저장 중 문제가 발생하였습니다.",
+            status: "error",
+          });
+        }
       })
       .finally(() => console.log("done"));
   }
